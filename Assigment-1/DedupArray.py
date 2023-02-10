a8b5d0b09742c93911b473ab5fb54ed30fe64f14
Emma <emmaxiao@my.yorku.ca>
2023-02-10 14:30:57 -0500

    modify q9

--- Assigment-1/DedupArray.py
+++ Assigment-1/DedupArray.py
@@ -8,21 +8,28 @@ def DedupArray(arr):
         return arr
     left = 1
     right = 1
+    n = len(arr)
     # move right pointer to check elements in the array, when element at right pointer is not a duplicate element,
     # keep it in the left pointer, and move left pointer.
     # this method makes sure that all the elements before left pointer is unique.
-    while right < len(arr):
+    while right < n:
         if arr[left - 1] != arr[right]: # check whether element at j is not a duplicate element
             arr[left] = arr[right]
             left += 1
         right += 1
-    return arr[0:left]
+    # delete the extra space from left pointer.
+    while left < n:
+        del arr[-1]
+        left += 1
 
 
 if __name__ == "__main__":
     arr_input = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
-    print(DedupArray(arr_input))
+    DedupArray(arr_input)
+    print(arr_input)
     arr_input = [0, 0, 1, 4, 5, 5, 5, 8, 9, 9, 10, 11, 15, 15]
-    print(DedupArray(arr_input))
+    DedupArray(arr_input)
+    print(arr_input)
     arr_input = [1, 3, 4, 8, 10, 12]
-    print(DedupArray(arr_input))
+    DedupArray(arr_input)
+    print(arr_input)
